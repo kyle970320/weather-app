641309ecf9127c0595a4828985c77ce24b3c73d6
kyle <park970320@gmail.com>
2026-01-25 16:42:43 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--color-background)",
+        foreground: "var(--color-foreground)",
+        "sub-bg": "var(--color-sub-bg)",
+        main: "var(--color-main)",
+      },
+    },
+  },
+  plugins: [],
+};
