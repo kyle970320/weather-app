8ef6d09f64aa635755e47adbca763eb7f4905497
minkyu_Park <kyle970320@farmzonesnc.com>
2026-01-26 17:51:42 +0900

    feat: add navigation to detail page on search

--- src/app/Layout/ui/Layout.tsx
+++ src/app/Layout/ui/Layout.tsx
@@ -1,12 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import SearchLocation from "@/widgets/searchLocation";
 import { useLayout } from "@/app/Layout/model/useLayout";
 import { useSearchModel } from "@/widgets/searchLocation/model/useSearchLocation";
 
 export default function Layout() {
-  const { search, onChangeSearch } = useLayout();
-  const { isFocused, onSearchFocus, suggestions, handleSuggestionClick } =
-    useSearchModel({ search, onChangeSearch });
+  const {
+    search,
+    onChangeSearch,
+    handleSearchLocation,
+    weatherData,
+    isWeatherLoading,
+    weatherError,
+    selectedLocation,
+  } = useLayout();
+
+  const navigate = useNavigate();
+  const {
+    isFocused,
+    onSearchFocus,
+    suggestions,
+    handleSuggestionClick: originalHandleSuggestionClick,
+  } = useSearchModel({ search, onChangeSearch });
+
+  //즉시 캐시에 담아서 router시 부드럽게 보여줌
+  const handleSuggestionClick = (suggestion: string) => {
+    originalHandleSuggestionClick(suggestion);
+    handleSearchLocation(suggestion);
+    navigate(`/${encodeURIComponent(suggestion)}`);
+  };
+
+  const onCustomSearchFocus = () => {
+    onChangeSearch("");
+    onSearchFocus();
+  };
+
+  const onSearchClick = () => {
+    handleSuggestionClick(search);
+  };
+
   return (
     <div className="p-14 min-h-screen mx-auto bg-linear-to-br from-main/70 via-main/90 to-main">
       <div className="max-w-[1200px] mx-auto">
@@ -14,13 +45,21 @@ export default function Layout() {
           placeholder="Search"
           className="bg-white/60 backdrop-blur-2xl shadow-lg"
           isFocused={isFocused}
-          onSearchFocus={onSearchFocus}
+          onSearchFocus={onCustomSearchFocus}
           searchValue={search}
           onChange={onChangeSearch}
           suggestions={suggestions}
           handleSuggestionClick={handleSuggestionClick}
+          onSearchClick={onSearchClick}
         />
-        <Outlet />
+        <Outlet
+          context={{
+            weatherData,
+            selectedLocation,
+            isWeatherLoading,
+            weatherError,
+          }}
+        />
       </div>
     </div>
   );
