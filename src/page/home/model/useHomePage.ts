b05cc724fb8a66a78a844271267ad5a8b867234f
minkyu_Park <kyle970320@farmzonesnc.com>
2026-01-26 16:21:32 +0900

    refactor: simplify useHomePage by removing widget dependency

--- src/page/home/model/useHomePage.ts
+++ src/page/home/model/useHomePage.ts
@@ -1,16 +1,3 @@
-import { useSearchLocation } from "@/widgets/searchLocation/model/useSearchLocation";
-
-/**
- * Home 페이지에서 사용하는 데이터와 로직
- */
 export function useHomePage() {
-  const { weatherData, selectedLocation, isLoading, error } =
-    useSearchLocation();
-
-  return {
-    weatherData,
-    selectedLocation,
-    isLoading,
-    error,
-  };
+  return {};
 }
