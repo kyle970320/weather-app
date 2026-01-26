a14dfe707fd885cb3a84733bc3bebe4465904b54
minkyu_Park <kyle970320@farmzonesnc.com>
2026-01-26 15:27:56 +0900

    chore: rename getSearchLocation to getLocationWithAddress

--- src/entity/location/model/useSearchLocationQuery.ts
+++ src/entity/location/model/useSearchLocationQuery.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { getSearchLocation } from "../api";
+import { getLocationWithAddress } from "../api";
 import type { SearchLocationParams } from "../types";
 
 export const useSearchLocationQuery = (params: SearchLocationParams) => {
   const { query, page, size, enabled = true } = params;
   return useQuery({
     queryKey: ["address", "search", query, page, size],
-    queryFn: () => getSearchLocation(params),
+    queryFn: () => getLocationWithAddress(params),
     enabled: enabled,
     staleTime: 1000 * 60 * 5,
     gcTime: 1000 * 60 * 10,
