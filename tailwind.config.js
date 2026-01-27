3d7e14afa298ea475ec0f98c14a582a2a7fce285
minkyu_Park <kyle970320@farmzonesnc.com>
2026-01-27 14:22:08 +0900

    chore: modify animation config to css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      animation: {
-        shimmer: "shimmer 2s infinite",
-      },
-      keyframes: {
-        shimmer: {
-          "100%": { transform: "translateX(100%)" },
-        },
-      },
       colors: {
         background: "var(--color-background)",
         foreground: "var(--color-foreground)",
         "sub-bg": "var(--color-sub-bg)",
         main: "var(--color-main)",
+        
       },
     },
   },
