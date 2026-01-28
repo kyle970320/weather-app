d826b3c9569fe9ef0690964279194e7a38a7daa2
minkyu_Park <kyle970320@farmzonesnc.com>
2026-01-28 10:56:37 +0900

    feat: add shared Button and Modal components

--- src/shared/lib/variants.ts
+++ src/shared/lib/variants.ts
@@ -1,7 +1,27 @@
 import clsx from "clsx";
 import type { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { cva } from "class-variance-authority";
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
+
+export const buttonVariants = cva(
+  "inline-flex items-center justify-center font-medium rounded-sm transition-colors cursor-pointer focus:outline-none  disabled:pointer-events-none",
+  {
+    variants: {
+      variant: {
+        primary:
+          "bg-[#2B7FFF] text-white disabled:bg-[#EDEEF9] disabled:text-[#B0B4D8] px-5 py-1 font-gmarket font-bold",
+        delete:
+          "bg-white border border-[#FF6467] text-[#FF6467] px-5 py-1 disabled:bg-[#F5F7F9] disabled:border-[#E8EEF2] disabled:text-[#C7D0D7] font-bold font-gmarket",
+        common:
+          "bg-[#F8F8F8] border border-[#C7C7C7] text-[#737373] px-4 py-1 font-gmarket font-bold",
+      },
+    },
+    defaultVariants: {
+      variant: "common",
+    },
+  },
+);
